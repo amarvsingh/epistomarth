f7f92dab88ac182d6426f4963e37f3d9a0e16412
Amar Singh <amar.vs@somaiya.edu>
2025-04-28 13:12:00 +0530

    authentication: Small changes (quotes)

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,2 @@
 //TODO: Implement the login function
-export async function loginWithEmailandPassword(email, password) {
-    
-    
-}
+export async function loginWithEmailandPassword(email, password) {}
